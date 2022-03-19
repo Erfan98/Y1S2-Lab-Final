3885e8a4fa76e7879d19c6fa8251b4c65b40dbd3
ftanvir <ft.tanvir551@gmail.com>
2022-03-19 21:30:10 +0600

    error fixed

--- 27.c
+++ 27.c
@@ -1,15 +1,17 @@
 // 1 + 3^2/3^3 + 5^2/5^3 + 7^2/7^3 + ... till N terms (use pow() function)
 
 #include<stdio.h>
+#include <math.h>
 int main(){
     int n;
     float i,sum=0;
     printf("Enter the N: ");
     scanf("%d",&n);
-    printf("1 + ");
-    for(i=3;i<=n;i+=2){
-        sum=sum+(pow(i,2)/pow(i,3));
-        printf("%d^2/%d^3 + ",(int)i,(int)i);
+    for(i=1;i<=n;i++){
+        int term;
+        term = 2*i-1;
+        sum=sum+(pow(term,2)/pow(term,3));
+        printf("%d^2/%d^3 + ",(int)term,(int)term);
     }
     printf("\b\b\b = %lf",sum);
     return 0;
