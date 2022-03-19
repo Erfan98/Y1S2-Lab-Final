b086effb70c42b1a522d707a15130341edf31c0e
ftanvir <ft.tanvir551@gmail.com>
2022-03-19 19:34:11 +0600

    error fixed

--- 81.c
+++ 81.c
@@ -1,32 +1,22 @@
-//Write a program in C to reverse array elements using pointer.
+//Write a program in C to compute sum of array elements using pointer.
 
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-	int size;
-	
-	scanf("%d", &size);
-	int a[100];
-	printf("Enter numbers: ");
-	for(int i=0; i<size; i++)
-		scanf("%d", &a[i]);
-    
-    int *p = a; //pointing to the first element
-	int *first = p;
-	int *last = p+size-1;
+    int i, size, *p,arr[100], sum = 0;
 
-    //swapping the last element with first element
-	while(first<last)
-	{
-		int temp = *first;
-		*first = *last;
-		*last = temp;
-		first++;
-		last--;
-	}
-	printf("Reversed array elements are: ");
-	for(int i=0; i<size; i++)
-		printf("%d ", *p++);
-	return 0;
+    scanf("%d", &size);
+    for(i=0; i<size; i++) {
+        scanf("%d", &arr[i]);
+    }
+    p = arr;
+
+    for(i = 0; i<size; i++) {
+        sum+=*p;
+        *p++;
+    }
+    printf("%d", sum);
+
+    return 0;
 }
--- 82.c
+++ 82.c
@@ -1,15 +1,32 @@
-//write a program in C to print array elements in reverse order using pointer
+//Write a program in C to reverse array elements using pointer.
 
-#include <stdio.h>
+#include<stdio.h>
 
 int main()
 {
-    int arr[100];
-    int size, i;
-    scanf("%d", &size);
-    for(i = 0; i<size; i++) {
-        scanf("%d", &arr[i]);
-    }
+	int size;
+	
+	scanf("%d", &size);
+	int a[100];
+	printf("Enter numbers: ");
+	for(int i=0; i<size; i++)
+		scanf("%d", &a[i]);
+    
+    int *p = a; //pointing to the first element
+	int *first = p;
+	int *last = p+size-1;
 
-    return 0;
+    //swapping the last element with first element
+	while(first<last)
+	{
+		int temp = *first;
+		*first = *last;
+		*last = temp;
+		first++;
+		last--;
+	}
+	printf("Reversed array elements are: ");
+	for(int i=0; i<size; i++)
+		printf("%d ", *p++);
+	return 0;
 }
